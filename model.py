992fb77f3b5c7178f894f3a38697ec1994bca8c9
AjitPandey-4 <76429747+AjitPandey-4@users.noreply.github.com>
2025-01-14 16:42:59 +0530

    Update model.py
    removed unwanted character

--- model.py
+++ model.py
@@ -14,5 +14,5 @@ model = LogisticRegression(max_iter=200)
 model.fit(X_train, y_train)
 
 # Print accuracy
-print("Model accuracy:", model.score(X_test, y_test))
+print("Model accuracy:", model.score(X_test, y_test))
 # added new line
